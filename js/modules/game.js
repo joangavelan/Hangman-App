703a13d3ebb69afb0f483f37ec6be42d23538550
Johan Gavelan <johangavelan@gmail.com>
2020-11-25 16:21:47 -0500

    game logic done

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,3 +1,6 @@
+import Home from "./home.js";
+import {sound} from "./../data/sound.js";
+
 const Game = ( _ => {
 
     const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -19,6 +22,72 @@ const Game = ( _ => {
         lives = 7;
         //show the initial screen
         showInitPage();
+        listeners();
+    }
+
+    const listeners = _ => {
+        $hangman.addEventListener('click', event => {
+            if(event.target.matches('.hangman__letter')) {
+                sound.click.play();
+                check(event.target.innerHTML);
+            }
+
+            if(event.target.matches('.hangman__trigger')) {
+                sound.click.play();
+                Home.init();
+            }
+        })
+    }
+
+    const isAlreadyTaken = letter => guesses.includes(letter);
+
+    const check = guess => {
+        if(isAlreadyTaken(guess)) return;
+
+        guesses.push(guess);
+
+        //check if the guess exists in chosenword
+        if(chosenWord.includes(guess)) {
+            //update the guessing word
+            updateGuessingWord(guess);
+        }
+        else {
+            lives--;
+            //render the board accordingly
+        }
+        render();
+        //check if the game is over
+        isGameOver();
+    }
+
+    const hasWon = _ => guessingWord.join("") === chosenWord;
+    const hasLost = _ => lives <= 0;
+
+    const isGameOver = _ => {
+        //if won, then alert("win");
+        if(hasWon()) {
+            sound.win.play();
+            alert('You won');
+        }
+        //if lost, then alert("lost");
+        if(hasLost()) {
+            sound.lose.play();
+            alert('You lose')
+        }
+    }
+
+    const render = _ => {
+        document.querySelector('.hangman__lives').innerHTML = lives;
+        document.querySelector('.hangman__word').innerHTML = guessingWord.join("");
+        document.querySelector('.hangman__letters').innerHTML = createLetters();
+    }
+
+    const updateGuessingWord = letter => {
+        chosenWord.split("").forEach((elem, index) => {
+            if(elem === letter) {
+                guessingWord[index] = elem;
+            }
+        })
     }
 
     const showInitPage = _ => {
@@ -42,10 +111,11 @@ const Game = ( _ => {
     const createLetters = _ => {
         let markup = '';
         letters.forEach(letter => {
+            const isActive = isAlreadyTaken(letter) ? 'hangman__letter--active' : '';
+            
             markup += `
-                <li class="hangman__letter">${letter}</li>
+                <li class="hangman__letter ${isActive}">${letter}</li>
             `
-
         })
         return markup;
     }
